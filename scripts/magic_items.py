767b319ba15002a3ff135cd9243b206ad15a24ba
Andrew Zhu <me@andrew-zhu.com>
2023-03-02 20:45:01 -0500

    misc: add magic item utils

---
+++ scripts/magic_items.py
@@ -0,0 +1,38 @@
+import csv
+import json
+import re
+
+RARITY_RE = re.compile("\Wcommon\W")
+
+
+def rarity_filter():
+    with open("../data/magic-items.json") as f:
+        items = json.load(f)
+    for item in items:
+        if item["isLegacy"]:
+            continue
+        if RARITY_RE.search(item["meta"]):
+            print(f'{item["name"]}, {item["source"]}')
+
+
+def csv_to_json():
+    default_cost = 50
+    default_days = 5
+    out = []
+    with open("../data/Magic Items - Common.csv") as f:
+        reader = csv.DictReader(f, ("name", "source", "cost", "days", "restrictions", "comment"))
+        next(reader)
+        for item in reader:
+            item["cost"] = int(item["cost"] or default_cost)
+            item["days"] = int(item["days"] or default_days)
+            item["restrictions"] = item["restrictions"] or None
+            item["comment"] = item["comment"] or None
+            if item["restrictions"] in ("Banned", "Restricted", "Community Goal"):
+                continue
+            out.append(item)
+    with open("../data/crafting-magic-items-common.json", "w") as f:
+        json.dump(out, f)
+
+
+if __name__ == "__main__":
+    csv_to_json()
