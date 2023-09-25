722d40195d3a95cd69d62489639cd6e2ad3947a2
Andrew Zhu <me@andrew-zhu.com>
2023-09-25 16:34:57 -0400

    add create thread button

--- calypso/cogs/onboarding.py
+++ calypso/cogs/onboarding.py
@@ -1,9 +1,10 @@
 import disnake
 from disnake.ext import commands
 
-from calypso import constants
+from calypso import constants, utils
 
 ONBOARDING_BUTTON_ID = "onboarding.agree"
+THREAD_BUTTON_ID = "onboarding.thread"
 
 
 class Onboarding(commands.Cog):
@@ -31,30 +32,66 @@ class Onboarding(commands.Cog):
         )
         await inter.send("ok", ephemeral=True)
 
-    @commands.Cog.listener()
-    async def on_button_click(self, interaction: disnake.MessageInteraction):
-        if interaction.guild_id != constants.GUILD_ID:
-            return
-        if interaction.data.custom_id != ONBOARDING_BUTTON_ID:
-            return
-        if not isinstance(interaction.author, disnake.Member):
-            return
+    async def on_onboard_click(self, interaction: disnake.MessageInteraction):
         member: disnake.Member = interaction.author
         if member.get_role(constants.MEMBER_ROLE_ID) is not None:
             return await interaction.send("You have already agreed to the rules.", ephemeral=True)
         # user is new, add the member role and welcome them in general
         await interaction.send(
-            (
-                "Welcome to the Northern Lights Province!\nYou now have access to the rest of the server. Come say"
-                f" hello in <#{constants.GENERAL_CHANNEL_ID}> and take a look at our resources channels (just below"
-                " this channel) to get started!"
-            ),
+            "Welcome to the Northern Lights Province!\nYou now have access to the rest of the server. Come say"
+            f" hello in <#{constants.GENERAL_CHANNEL_ID}> and take a look at our resources channels (just below"
+            " this channel) to get started!",
             ephemeral=True,
         )
         await member.add_roles(interaction.guild.get_role(constants.MEMBER_ROLE_ID), reason="Accepted rules")
         await interaction.guild.get_channel(constants.GENERAL_CHANNEL_ID).send(
             f"Welcome to the Northern Lights Province, {member.mention}!"
         )
+
+    # thread
+    @commands.slash_command(
+        description="Sends a new button to create a private thread, in this channel.", dm_permission=False
+    )
+    @commands.default_member_permissions(manage_guild=True)
+    async def send_thread_button(
+        self,
+        inter: disnake.MessageCommandInteraction,
+        label: commands.String[1, 80] = None,
+        emoji: str = None,
+        style: disnake.ButtonStyle = disnake.ButtonStyle.primary.value,
+    ):
+        if label is None:
+            label = "Create thread"
+        if emoji is not None:
+            emoji = disnake.PartialEmoji.from_str(emoji)
+        await inter.channel.send(
+            components=disnake.ui.Button(
+                style=disnake.ButtonStyle(style), emoji=emoji, label=label, custom_id=THREAD_BUTTON_ID
+            )
+        )
+        await inter.send("ok", ephemeral=True)
+
+    async def on_thread_click(self, interaction: disnake.MessageInteraction):
+        thread_name = utils.smart_trim(f"Character Submission: {interaction.author.name}", max_len=80)
+        channel = interaction.channel
+        thread = await channel.create_thread(
+            name=thread_name,
+            type=disnake.ChannelType.private_thread,
+            auto_archive_duration=1440,
+        )
+        await thread.add_user(interaction.author)
+
+    # listener
+    @commands.Cog.listener()
+    async def on_button_click(self, interaction: disnake.MessageInteraction):
+        if interaction.guild_id != constants.GUILD_ID:
+            return
+        if not isinstance(interaction.author, disnake.Member):
+            return
+        if interaction.data.custom_id == ONBOARDING_BUTTON_ID:
+            return await self.on_onboard_click(interaction)
+        if interaction.data.custom_id == THREAD_BUTTON_ID:
+            return await self.on_thread_click(interaction)
 
 
 def setup(bot):
---
+++ scripts/prune.py
@@ -0,0 +1,45 @@
+"""
+Kick all members who have been in the server longer than one week unless they have any role in NO_PRUNE_IF.
+"""
+import asyncio
+import datetime
+import os
+
+import disnake
+
+SERVER_ID = 1031055347319832666
+NO_PRUNE_IF = {
+    1031277823404560404,  # Player
+    1031340755710652507,  # Server Booster
+    1033514863005274203,  # Bots
+    1149379623826751538,  # No Prune
+}
+
+client = disnake.Client(intents=disnake.Intents.all())
+
+
+@client.event
+async def on_ready():
+    print("ready")
+    guild = client.get_guild(SERVER_ID)
+    print(len(guild.members), "members")
+    for member in guild.members.copy():
+        if member.joined_at > disnake.utils.utcnow() - datetime.timedelta(days=7):
+            continue
+        if not NO_PRUNE_IF.intersection(r.id for r in member.roles):
+            print("pruning", member.display_name)
+            try:
+                await member.send(
+                    "You have been removed from the Northern Lights Province server, as you have been inactive for over"
+                    " one week without creating a character.\n\nIf you would like to rejoin, please use the following"
+                    " invite: https://discord.gg/RGDTtrhceC"
+                )
+            except:
+                pass
+            await member.kick(reason="inactivity prune")
+            await asyncio.sleep(1.5)
+    await client.close()
+
+
+if __name__ == "__main__":
+    client.run(os.getenv("TOKEN"))
